f7bd67d440e7c8938a730a004d67d2c87c936a48
Paulchen Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2021-03-21 17:09:32 +0000

    mobile changes

--- docs/.vuepress/theme/components/NavLinks.vue
+++ docs/.vuepress/theme/components/NavLinks.vue
@@ -1,7 +1,44 @@
 <template>
-  <div class="theme-toggler" v-if="!isMobile">
-    <a @click="toggle">
+  <nav
+    v-if="userLinks.length || repoLink"
+    class="nav-links"
+  >
+    <!-- user links -->
+    <div
+      v-for="item in userLinks"
+      :key="item.link"
+      class="nav-item"
+    >
+      <DropdownLink
+        v-if="item.type === 'links'"
+        :item="item"
+      />
+      <NavLink
+        v-else
+        :item="item"
+      />
+    </div>
 
+    <!-- repo link -->
+    <a
+      v-if="repoLink"
+      :href="repoLink"
+      class="repo-link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {{ repoLabel }}
+      <OutboundLink />
+    </a>
+
+    <!-- Dark Mode -->
+    <a
+      v-if="isMobile"
+      @click="toggle"
+      class="repo-link"
+      :title="repoLabel"
+      :aria-label="repoLabel"
+    >
       <svg v-if="isLight" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon">
         <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
       </svg>
@@ -17,13 +54,16 @@
         <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
         <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
       </svg>
-
     </a>
-  </div>
+  </nav>
 </template>
 
 <script>
+  import ParentNavLinks from '@parent-theme/components/NavLinks.vue'
+  import ThemeToggler from './ThemeToggler';
   export default {
+    extends: ParentNavLinks,
+    components: { ThemeToggler },
     data: () => ({
       isMobile: false,
       isLight: false
@@ -39,9 +79,6 @@
       this.onResize();
       window.addEventListener('resize', this.onResize, { passive: true });
       this.isLight = window.localStorage.getItem('hyperion_light') === 'on';
-      if (!this.isLight) {
-        document.getElementsByTagName('html')[0].classList.add('dark');
-      }
     },
 
     methods: {
@@ -55,18 +92,10 @@
       onResize () {
         this.isMobile = window.innerWidth < 720
       }
+    },
+
+    created() {
+      this.$root.$on('light-mode', (isLight) => this.isLight = isLight);
     }
   }
 </script>
-
-<style scoped>
-  div {
-    margin-right: 1.5rem;
-    padding-top: 6px;
-  }
-  a {
-    cursor: pointer;
-    color: inherit;
-  }
-</style>
-
